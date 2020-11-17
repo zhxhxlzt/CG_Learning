7b046f1895ac931bac51dcd599cd9dce7ed0d821
liu yiki <2284174996@qq.com>
2020-11-18 01:38:10 +0800

    ray计算错误fix

--- Code/engine/Public/Camera.cpp
+++ Code/engine/Public/Camera.cpp
@@ -6,7 +6,7 @@
 CRay Camera::GetRay(const glm::vec2& screenRatio)
 {
 	using namespace glm;
-	vec4 v0 = { -0.5f + 0.5f * screenRatio.x, -0.5f + 0.5f * screenRatio.y, -1, 1 };
+	vec4 v0 = { -1.0f + 2.0f * screenRatio.x, -1.0f + 2.0f * screenRatio.y, -1, 1 };
 	mat4 inv_pv = inverse(GetPerspectiveProjectionMatrix() * GetViewMatrix());
 	vec3 v1 = inv_pv * v0;
 	vec3 v2 = v1 - Position;
